fd9e37f94f202baf7cc05c2b6c8f25d18696bbf7
Tim-Hopanchuk <tim.hpnk@gmail.com>
2025-06-28 20:23:38 +0300

    feat: implement promise utils

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,53 @@
 "use strict";
 
+export function sleep(timeout: number) {
+  return new Promise<void>((resolve, reject) => {
+    setTimeout(resolve, timeout);
+  });
+}
+
+export function wait(callback: () => Promise<unknown>, timeout: number) {
+  return Promise.race([
+    new Promise((resolve, reject) => {
+      setTimeout(() => reject("Rejected: timeout"), timeout);
+    }),
+    callback(),
+  ]);
+}
+
+export async function waitFor(
+  callback: () => unknown,
+  timeout: number,
+  count: number
+) {
+  for (let i = 0; i < count; i++) {
+    try {
+      if (callback()) {
+        return "Resolved: callback";
+      }
+    } catch {
+      return Promise.reject("Rejected: callback error");
+    }
+    
+    await sleep(timeout);
+  }
+
+  return Promise.reject("Rejected: count");
+}
+
+export async function doCallbacks(
+  callbacks: Array<() => unknown>,
+  timeout: number
+) {
+  for (let callback of callbacks) {
+    try {
+      await callback();
+    } catch {
+      return Promise.reject("Callback: error");
+    }
+
+    await sleep(timeout);
+  }
+
+  return "good";
+}
