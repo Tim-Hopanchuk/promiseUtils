51577ab694919ad6897871428567ab069edbc88d
Tim-Hopanchuk <tim.hpnk@gmail.com>
2025-06-29 14:23:56 +0300

    docs: add JSDoc annotations

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
 "use strict";
 
+/**
+ * Returns a promise that resolves after a timeout.
+ *
+ * @param {number} timeout - Milliseconds before resolving.
+ * @returns {Promise<void>} A promise that resolves after the timeout.
+ */
+
 export function sleep(timeout: number): Promise<void> {
   return new Promise<void>((resolve, reject) => {
     setTimeout(resolve, timeout);
   });
 }
+
+/**
+ * Executes an async callback and returns a promise that resolves with its result.
+ * Rejects if the timeout is reached first.
+ *
+ * @param {() => Promise<T>} callback - An async function to execute.
+ * @param {number} timeout - Milliseconds before rejecting.
+ * @returns {Promise<string | T>} A promise that resolves with the callback's result or rejects on timeout.
+ */
 
 export function wait<T>(
   callback: () => Promise<T>,
@@ -17,6 +33,16 @@ export function wait<T>(
     callback(),
   ]);
 }
+
+/**
+ * Repeatedly calls a function until it returns true, or a max number of attempts is reached. 
+ * Has a delay between calls.
+ *
+ * @param {() => boolean} callback - A sync function to execute.
+ * @param {number} timeout - Milliseconds between calls.
+ * @param {number} tries - Max number of attempts.
+ * @returns {Promise<string>} A promise that resolves when callback returns true, or rejects on failure or Max number of attempts reached.
+ */
 
 export async function waitFor(
   callback: () => boolean,
@@ -36,6 +62,14 @@ export async function waitFor(
   }
   return Promise.reject("Rejected: max retries");
 }
+
+/**
+ * Executes a list of functions sequentially with a delay between calls.
+ *
+ * @param {Array<() => T | Promise<T>>} callbacks - An array of sync or async functions to execute.
+ * @param {number} timeout - Milliseconds between calls.
+ * @returns {Promise<string | T>}  A promise that resolves if all functions succeed, or rejects if any function fails.
+ */
 
 export async function doCallbacks<T>(
   callbacks: Array<() => T | Promise<T>>,
